01fc8c8aae3e92c415981f4b0045bf0636f4f867
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-20 14:06:43 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Question and Answer site
+
+**An app a day keeps the skills sharp!** A simple Rails based collaborative question-and-answer site.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionAndAnswer::Application.config.secret_token = '8826100c0bdcb6dfe03942157fe5b677209a5d594609f75c237896989d187ccefbb436131e0656d511f34ed81c256b8f7b02ff0bbdaf0da4b0157d6b7f031147'
